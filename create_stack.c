f28d5b9c3dd0b2d96df35aa3eae1e389ab4dbe21
betulyildizs <120586474+betulyildizs@users.noreply.github.com>
2024-05-02 19:42:10 +0300

    Add files via upload

---
+++ create_stack.c
@@ -0,0 +1,89 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: beyildiz <beyildiz@student.42istanbul.c    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/04/25 13:05:20 by beyildiz          #+#    #+#             */
+/*   Updated: 2024/05/02 19:39:36 by beyildiz         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "push_swap.h"
+#include "ft_printf.h"
+
+/*void	print_stack(Stack a)
+{
+	int	i;
+
+	i = 0;
+	ft_printf("Stack contents:\n");
+	while (i <= a.top) 
+	{
+		ft_printf("%d\n", a.values[i]);
+		i++;
+	}
+}*/
+
+int	push_onto_stack(int argc, char *argv[], Stack a)
+{
+	int	i;
+
+	i = 1;
+	while (i < argc)
+	{
+		a.values[++a.top] = ft_atoi(argv[i]);
+		if (a.top > MAX_SIZE - 1)
+			return (1);
+		i++;
+	}
+	print_stack(a);
+	return (0);
+}
+
+int	check_number(char *str, int argc, char **argv, Stack a)
+{
+	int	i;
+
+	i = 0;
+	while (str[i] != '\0')
+	{
+		if (str[i] == '-')
+		{
+			if (str[i + 1] > '9' || str[i + 1] < '0')
+			{
+				ft_printf("Error\n");
+				return (0);
+			}
+			i++;
+		}
+		else if (str[i] > '9' || str[i] < '0')
+		{
+			ft_printf("ERROR\n");
+			return (0);
+		}
+		i++;
+	}
+	return (push_onto_stack(argc, argv, a));
+}
+
+int	main(int argc, char *argv[])
+{
+	Stack	a;
+	int		i;
+
+	if (argc < 0 || argc == 1)
+	{
+		ft_printf("error\n");
+		return (0);
+	}
+	a.top = -1;
+	i = 1;
+	while (argv[i])
+	{
+		check_number(ft_strdup(argv[i]), argc, argv, a);
+		i++;
+	}
+	return (0);
+}
