66dbbc778203c8184f1450e724e3981b14e4b32e
betulyildizs <120586474+betulyildizs@users.noreply.github.com>
2024-05-02 17:11:14 +0300

    Add files via upload

--- main.c
+++ main.c
@@ -6,15 +6,14 @@
 /*   By: beyildiz <beyildiz@student.42istanbul.c    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/25 13:05:20 by beyildiz          #+#    #+#             */
-/*   Updated: 2024/04/29 15:03:53 by beyildiz         ###   ########.fr       */
+/*   Updated: 2024/04/30 16:13:44 by beyildiz         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 #include "ft_printf.h"
 
-
-/*void print_stack(Stack a)
+void print_stack(Stack a)
 {
 	int	i;
 
@@ -25,7 +24,7 @@
 		ft_printf("%d\n", a.values[i]);
 		i++;
 	}
-}*/
+}
 
 int	push_onto_stack(int argc, char *argv[], Stack a)
 {
@@ -43,50 +42,53 @@ int	push_onto_stack(int argc, char *argv[], Stack a)
 	return (0);
 }
 
-
-int	check_number(Stack a)
+int	check_number(char *str)
 {
 	int	i;
 
 	i = 0;
-	while (a.values[i])
+	while (str[i])
 	{
-		if (a.values[i] == '-')
+		if (str[i] == '-')
 		{
-			if (a.values[i++] > '9' && a.values[i++] < '0')
+			if (str[++i] <= '9' && str[++i] >= '0')
+				i++;
+			else
 			{
 				ft_printf("Error\n");
 				return (0);
 			}
-			else
-				i++;
 		}
-		else if (a.values[i] <= '9' && a.values[i] >= '0')
+		else if (str[i] <= '9' && str[i] >= '0')
 			i++;
 		else
 		{
-			ft_printf("Error\n");
+			ft_printf("ERROR\n");
 			return (0);
 		}
 	}
-	return (0);
+	return (1);
 }
 
 int	main(int argc, char *argv[])
 {
 	Stack	a;
+	int		i;
+	int		j;
 
 	if (argc < 0 || argc == 1)
 	{
-		ft_printf("Error\n");
+		ft_printf("error\n");
 		return (0);
 	}
-	a.top = -2;
-	push_onto_stack(argc, argv, a);
-	check_number(a);
-	return (0);
-	/*while ()
+	a.top = -1;
+	i = 0;
+	j = 0;
+	while (argv[i])
 	{
-		ft_
-	}*/
+		check_number(ft_strdup(argv[i]));
+		i++;
+	}
+	push_onto_stack(argc, argv, a);
+	return (0);
 }
--- push_swap_utils.c
+++ push_swap_utils.c
@@ -6,7 +6,7 @@
 /*   By: beyildiz <beyildiz@student.42istanbul.c    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/25 13:05:14 by beyildiz          #+#    #+#             */
-/*   Updated: 2024/04/29 14:25:52 by beyildiz         ###   ########.fr       */
+/*   Updated: 2024/04/30 16:08:42 by beyildiz         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -33,4 +33,46 @@ int	ft_atoi(char *str)
 		i++;
 	}
 	return (result * sign);
+}
+
+size_t	ft_strlen(const char *str)
+{
+	unsigned int	i;
+
+	i = 0;
+	while (str[i] != '\0')
+	{
+		i++;
+	}
+	return (i);
+}
+
+size_t	ft_strlcpy(char *dest, const char *src, size_t size)
+{
+	unsigned int	i;
+	size_t			lenght;
+
+	lenght = ft_strlen(src);
+	i = 0;
+	if (size != 0)
+	{
+		while (src[i] != '\0' && i < size - 1)
+		{
+			dest[i] = src[i];
+			i++;
+		}
+		dest[i] = '\0';
+	}
+	return (lenght);
+}
+
+char	*ft_strdup(const char *str)
+{
+	char	*string;
+
+	string = (char *)malloc(sizeof(char) * (ft_strlen(str) + 1));
+	if (string == NULL)
+		return (0);
+	ft_strlcpy(string, str, ft_strlen(str) + 1);
+	return (string);
 }
