47f4dda9261c27e26dccbc36bd949e980bd29da5
Betül Yıldız <120586474+betulyildizs@users.noreply.github.com>
2024-06-23 21:37:57 +0300

    Add files via upload

--- push_swap.h
+++ push_swap.h
@@ -1,37 +1,67 @@
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                        :::      ::::::::   */
-/*   push_swap.h                                        :+:      :+:    :+:   */
-/*                                                    +:+ +:+         +:+     */
-/*   By: beyildiz <beyildiz@student.42istanbul.c    +#+  +:+       +#+        */
-/*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/04/25 13:05:17 by beyildiz          #+#    #+#             */
-/*   Updated: 2024/05/07 16:46:54 by beyildiz         ###   ########.fr       */
-/*                                                                            */
-/* ************************************************************************** */
-
 #ifndef PUSH_SWAP_H
+# define PUSH_SWAP_H
 
-#define PUSH_SWAP_H
+# include <unistd.h>
+# include <stdlib.h>
 
-#define MAX_SIZE 11000
-#define INT_MAX 2147483647
-#define INT_MIN -2147483648
-
-#include <stdlib.h>
-
-typedef	struct
+typedef struct s_list
 {
-	int	values[MAX_SIZE];
-	int	top;
-} Stack;
+	int				value;
+	int				index;
+	struct s_list	*next;
+}				t_list;
 
-int		ft_atoi(char *str);
-int		push_onto_stack(int argc, char *argv[], Stack a);
-int		check_number(char *str, int argc, char **argv, Stack a);
-char	*ft_strdup(const char *str);
-size_t	ft_strlcpy(char *dest, const char *src, size_t size);
-size_t	ft_strlen(const char *str);
-int		shell_sort(int n, Stack *a);
+static void     create_stack(t_list **stack_a, int argc, char **argv);
+static size_t	ft_toklen(const char *s, char c);
+char	        *ft_strdup(const char *str);
+char	        *ft_substr(char const *s, unsigned int start, size_t len);
+void            split_cond(const char *s, char **ret, char c, size_t  i);
+char	        **ft_split(const char *s, char c);
+size_t	        ft_strlen(const char *a);
+size_t	        ft_strlcpy(char *dest, const char *src, size_t size);
+t_list	        *ft_lstnew(int value);
+long	        ft_atoi(const char *str);
+t_list	        *ft_lstlast(t_list *head);
+void	        ft_lstadd_back(t_list **stack, t_list *new);
+void	        ft_lstadd_front(t_list **stack, t_list *new);
+int	            ft_lstsize(t_list *head);
+void	        printList(t_list *head);
+void	        ft_putchar_fd(char c, int fd);
+void	        ft_putstr_fd(const char *str, int fd);
+void	        ft_putendl_fd(char *str, int fd);
+void	        ft_putnbr_fd(int n, int fd);
+void            *ft_bzero(void *s, size_t len);
+int             ft_abs(int num);
+static t_list	*get_next_min(t_list **stack);
+void	        index_stack(t_list **stack);
+void	        ft_free(char **str);
+void	        free_stack(t_list **stack);
+int	            sort_cont(t_list **stack);
+void	        simple_sort(t_list **stack_a, t_list **stack_b);
+void	        sort_5(t_list **stack_a, t_list **stack_b);
+static void	    sort_4(t_list **stack_a, t_list **stack_b);
+static void	    sort_3(t_list **stack_a);
+static int	    get_min(t_list **stack, int val);
+static void     sort_3_cond(t_list **stack_a, int min, int next_min);
+int		        swap(t_list **stack);
+int		        push(t_list **stack_to, t_list **stack_from);
+int		        rotate(t_list **stack);
+int		        reverseRotate(t_list **stack);
+//instructions:
+int		        sa(t_list **stack_a);
+int		        sb(t_list **stack_b);
+int		        ss(t_list **stack_a, t_list **stack_b);
+int		        pa(t_list **stack_a, t_list **stack_b);
+int		        pb(t_list **stack_b, t_list **stack_a);
+int		        ra(t_list **stack_a);
+int		        rb(t_list **stack_b);
+int		        rr(t_list **stack_a, t_list **stack_b);
+int		        rra(t_list **stack_a);
+int		        rrb(t_list **stack_b);
+int		        rrr(t_list **stack_a, t_list **stack_b);
+//check_argv:
+static int	    ft_contains(int num, char **argv, int i);
+static int		ft_isnum(char *num);
+void			check_argv(int argc, char **argv);
 
 #endif
